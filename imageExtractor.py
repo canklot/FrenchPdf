cfe53bea4dc86a2c8d5d23e93e927749d87cd8f1
canklot <mustafacandan211@gmail.com>
2022-04-23 18:39:51 +0300

    commenxt extractor

--- imageExtractor.py
+++ imageExtractor.py
@@ -45,21 +45,16 @@ def imageExtract(filename):
             for img in page_images:
                 xref = img[0]
                 pix = fitz.Pixmap(doc, xref)
-                elif pix.n == 1:
-                    # If the image is grey    
+                elif pix.n < 5: # this is GRAY or RGB
                     pix = np.frombuffer(buffer=pix.samples, dtype=np.uint8).reshape((pix.height, pix.width, -1))
                     mymax = pix.max()
                     if pix.max() == 255 :
                         # Some grey images has 255 as white. Scale them to between 1 and 0
                         pix = (pix - np.min(pix))/np.ptp(pix)
-                elif pix.n < 5: # this is GRAY or RGB
-                    pix = np.frombuffer(buffer=pix.samples, dtype=np.uint8).reshape((pix.height, pix.width, 3))
-                else:               # CMYK: convert to RGB first
+                else:# CMYK: convert to RGB first
                     pix1 = fitz.Pixmap(fitz.csRGB, pix)
-                    #pix =  pix.pil_tobytes(format="JPG", optimize=True)
+                    #pix =  pix.pil_tobytes(format="JPG", optimize=True) # If you need PIL image. But I am using numpy array
                     pix = np.frombuffer(buffer=pix.samples, dtype=np.uint8).reshape((pix.height, pix.width, 3))
-                    #pix1.writePNG("p%s-%s.jpg" % (i, xref))
-                    #pix1 = None
 
                 """ plt.imshow(pix)
                 plt.title('extract')
@@ -70,10 +65,30 @@ def imageExtract(filename):
                 pix = None
     return imageList
 
+def add_margin_bbox(imageBox,margin_percent):
+    # Tesseract perform better with some margin around the text
+    # So add some margin on bounding box of cropped image
+    # Info: PIL coordinates start from left upper corner (0,0)
+    left = imageBox[0]
+    up = imageBox[1]
+    right = imageBox[2]
+    down = imageBox[3]
+    margin_amount_horizontal = (right - left) / 100 * margin_percent
+    margin_amount_vertical = (down - up) / 100 * margin_percent
+
+    new_left = max(1,left - margin_amount_horizontal)
+    new_right = min(im.shape[1],right + margin_amount_horizontal)
+    new_up= min(1,up + margin_amount_vertical)
+    new_down =max(im.shape[0],down - margin_amount_vertical)
+    imageBox_new = (new_left,new_up,new_right,new_down)
+    return imageBox_new
+
 def crop_convex_hull(im):
     # Crops white spaces in the image
     if im.shape[2] == 3:
+        If its rgb image turn it to gray
         im = rgb2gray(im)
+    # There are a lot of plt.show calls because of debuging
     """ plt.imshow(im,cmap="gray")
     plt.title('amigray')
     plt.show() """
@@ -81,7 +96,7 @@ def crop_convex_hull(im):
     """ plt.imshow(im1,cmap="gray")
     plt.title('after substraction')
     plt.show() """
-    # do some blur or closing etc
+    # do some blur or closing etc here for better cropping
     threshold = 0.65
     im1[im1 <= threshold] = 0
     im1[im1 > threshold] = 1
@@ -89,37 +104,30 @@ def crop_convex_hull(im):
     plt.title('after threshold')
     plt.show() """
     chull = convex_hull_image(im1)
+    
     plt.imshow(chull)
     plt.title('convex hull in the binary image')
     plt.show()
+
     imageBox = PIL.Image.fromarray((chull*255).astype(np.uint8)).getbbox()
-    # add margin
-    left = imageBox[0]
-    up = imageBox[1]
-    right = imageBox[2]
-    down = imageBox[3]
-    margin_percent = 10
-    margin_amount_horizontal = (right - left) / 100 * margin_percent
-    margin_amount_vertical = (down - up) / 100 * margin_percent
-    
-    new_left = max(1,left - margin_amount_horizontal)
-    new_right = min(im.shape[1],right + margin_amount_horizontal)
-    new_up= min(1,up + margin_amount_vertical)
-    new_down =max(im.shape[0],down - margin_amount_vertical)
-    imageBox_new = (new_left,new_up,new_right,new_down)
+    imageBox_new = add_margin_bbox(imageBox,10)
     cropped = PIL.Image.fromarray(im).crop(imageBox_new)
     cropped = np.asarray(cropped)
+
     plt.imshow(cropped)
     plt.title('crop_convex_hull')
     plt.show()
+
     return cropped
 
 def white_borders_skimage(img,thickness):
     img_height=img.shape[0]
     img_width=img.shape[1]
     percent_to_pix = int(img_width * thickness / 100)
+    # To save space I was going to use skimage not OpenCV but didnt finished it
     
 def white_borders(img,thickness):
+    # Draw white line on the borders of the image to cover up thick lines in some ID cards like cameroon
     img_height=img.shape[0]
     img_width=img.shape[1]
     percent_to_pix = int(img_width * thickness / 100)
